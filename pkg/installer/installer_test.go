86bb2d37063b2de5e8da0f64dad4bfa86e1fa64c
Dustin Davis <1dustindavis@gmail.com>
2019-03-23 13:19:19 -0700

    Wrote a test to ensure we dont lose the command output again in the future

--- pkg/installer/installer_test.go
+++ pkg/installer/installer_test.go
@@ -2,6 +2,7 @@ package installer
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -532,4 +533,38 @@ func TestUninstallURL(t *testing.T) {
 	if have, want := installItemURL, expectedURL; have != want {
 		t.Errorf("\n-----\nhave\n%s\nwant\n%s\n-----", have, want)
 	}
+}
+
+// ExampleRunCommandDebug tests the output when running a command in debug
+func ExampleRunCommandDebug() {
+	// Temp directory for logging
+	logTmp, _ := ioutil.TempDir("", "gorilla-installer_test")
+
+	// Setup a testing Configuration struct with debug more
+	cfgVerbose := config.Configuration{
+		Debug:       true,
+		Verbose:     true,
+		AppDataPath: logTmp,
+	}
+
+	// Start gorillalog in debug mode
+	gorillalog.NewLog(cfgVerbose)
+
+	// Override execCommand with our fake version
+	execCommand = fakeExecCommand
+	defer func() { execCommand = origExec }()
+
+	// Set up what we expect
+	testCmd := "Command Test!"
+	testArgs := []string{"arg1", "arg2"}
+
+	// Run the function
+	runCommand(testCmd, testArgs)
+
+	// Output:
+	// command: Command Test! [arg1 arg2]
+	// Command Output:
+	// --------------------
+	// [Command Test! arg1 arg2]
+	// --------------------
 }

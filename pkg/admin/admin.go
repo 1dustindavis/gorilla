3d058545823b2f9b37048c3d48c71657432dfa5e
Dustin Davis <1dustindavis@gmail.com>
2020-02-01 11:41:53 -0800

    Create new admin pkg for managing a gorilla repo

---
+++ pkg/admin/admin.go
@@ -0,0 +1,16 @@
+package admin
+
+import (
+	"github.com/1dustindavis/gorilla/pkg/catalog"
+	"github.com/1dustindavis/gorilla/pkg/config"
+)
+
+// BuildCatalogs iterates through the files in the `packages-info` directory and attempts to compile them into catalog files
+func BuildCatalogs(cfg config.Configuration) {
+
+}
+
+// ImportItem attempts to create a yaml file in the `packages-info` directory
+func ImportItem(cfg config.Configuration, item catalog.Item) {
+
+}

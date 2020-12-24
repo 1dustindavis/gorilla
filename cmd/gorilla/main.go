00bd0212b4f90e5bcd6e4668aa9105a97ed7c540
Bk <kurtz.brandon@gmail.com>
2020-12-24 12:41:42 -0500

    check if we are running as admin with syscall instead of powershell

--- cmd/gorilla/main.go
+++ cmd/gorilla/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
-	"os/exec"
 	"path/filepath"
-	"strings"
+
+	"golang.org/x/sys/windows"
 
 	"github.com/1dustindavis/gorilla/pkg/catalog"
 	"github.com/1dustindavis/gorilla/pkg/config"
@@ -23,10 +22,14 @@ func main() {
 	cfg := config.Get()
 
 	// Confirm we are running as an administrator before continuing
-	adminCheck(cfg.CheckOnly)
+	admin, err := adminCheck(cfg.CheckOnly)
+	if !admin {
+		fmt.Println("Gorilla requires admnisistrative access. Please run as an administrator.")
+		os.Exit(1)
+	}
 
 	// If needed, create the cache directory
-	err := os.MkdirAll(filepath.Clean(cfg.CachePath), 0755)
+	err = os.MkdirAll(filepath.Clean(cfg.CachePath), 0755)
 	if err != nil {
 		fmt.Println("Unable to create cache directory: ", err)
 		os.Exit(1)
@@ -87,40 +90,32 @@ func main() {
 	gorillalog.Info("Done!")
 }
 
-func adminCheck(CheckOnly bool) {
+func adminCheck(CheckOnly bool) (bool, error) {
+	var adminSid *windows.SID
 
-	// Skip the check if this is test
-	if flag.Lookup("test.v") != nil {
-		return
-	}
-
-	// Skip the check if checkonly mode is enabled
-	if CheckOnly {
-		return
-	}
-
-	// Compile the PowerShell command used to determine if the current user is an administrator
-	currentUser := "(New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent()))"
-	adminRole := "([Security.Principal.WindowsBuiltInRole]::Administrator)"
-	checkCmd := currentUser + ".IsInRole" + adminRole
-
-	// Execute the command with Powershell and capture the output
-	cmdOutput, err := exec.Command("powershell.exe", "-Command", checkCmd).CombinedOutput()
+	// Although this looks scary, it is directly copied from the
+	// official windows documentation. The Go API for this is a
+	// direct wrap around the official C++ API.
+	// See https://docs.microsoft.com/en-us/windows/desktop/api/securitybaseapi/nf-securitybaseapi-checktokenmembership
+	err := windows.AllocateAndInitializeSid(
+		&windows.SECURITY_NT_AUTHORITY,
+		2,
+		windows.SECURITY_BUILTIN_DOMAIN_RID,
+		windows.DOMAIN_ALIAS_RID_ADMINS,
+		0, 0, 0, 0, 0, 0,
+		&adminSid)
 	if err != nil {
-		fmt.Println("Unable to determine current permissions via Powershell: ", err)
-		fmt.Println("Gorilla requires admnisistrative access. Please run as an administrator.")
-		os.Exit(1)
+		return false, fmt.Errorf("SID Error: %v", err)
 	}
+	defer windows.FreeSid(adminSid)
+	// This appears to cast a null pointer so I'm not sure why this
+	// works, but this guy says it does and it Works for Me™:
+	// https://github.com/golang/go/issues/28804#issuecomment-438838144
+	token := windows.Token(0)
 
-	// Convert the output to a lowercase string
-	strOutput := strings.ToLower(string(cmdOutput))
-
-	// If the output contains the word "true", we are running as an administrator
-	if strings.Contains(strOutput, "true") {
-		return
+	admin, err := token.IsMember(adminSid)
+	if err != nil {
+		return false, fmt.Errorf("Token Membership Error: %v", err)
 	}
-
-	// The user does not have the `Administrator` role
-	fmt.Println("Gorilla requires admnisistrative access. Please run as an administrator.")
-	os.Exit(1)
+	return admin, nil
 }

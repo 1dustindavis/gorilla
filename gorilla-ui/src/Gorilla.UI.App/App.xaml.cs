cac527ca45dc157e10aee0d89208b8713fc6aca6
Dustin Davis <1dustindavis@gmail.com>
2026-09-11 17:13:31 -0700

    Establish shared App Catalog session

--- gorilla-ui/src/Gorilla.UI.App/App.xaml.cs
+++ gorilla-ui/src/Gorilla.UI.App/App.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using Gorilla.UI.App.Services;
-using Gorilla.UI.App.Views;
 using Gorilla.UI.Core.Services;
 using Gorilla.UI.Core.ViewModels;
 using Microsoft.UI.Xaml;
@@ -15,11 +14,15 @@ namespace Gorilla.UI.App
         private const string E2EPipeNameVariable = "GORILLA_UI_E2E_PIPE_NAME";
         private const string CachePathOverrideVariable = "GORILLA_UI_CACHE_PATH";
         private Window? _window;
+        private AppCatalogSession? _session;
 
         public App()
         {
             InitializeComponent();
         }
+
+        internal static AppCatalogSession CurrentSession
+            => ((App)Current)._session ?? throw new InvalidOperationException("App Catalog session is not initialized.");
 
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
@@ -28,10 +31,21 @@ namespace Gorilla.UI.App
             var services = GorillaUiServices.Create(cacheFilePath, pipeName);
             var operationTracker = new OperationTracker(services.Client);
             var homeViewModel = new HomeViewModel(services.Client, services.CacheCoordinator, operationTracker);
-            var homePage = new HomePage(homeViewModel);
+            _session = new AppCatalogSession(homeViewModel);
 
-            _window = new MainWindow(homePage);
+            _window = new MainWindow();
+            _window.Closed += Window_Closed;
             _window.Activate();
+        }
+
+        private void Window_Closed(object sender, WindowEventArgs args)
+        {
+            if (_window is not null)
+            {
+                _window.Closed -= Window_Closed;
+            }
+            _session?.Dispose();
+            _session = null;
         }
 
         private static string BuildCacheFilePath()

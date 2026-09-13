c550578f67d6ed869625add4f8b527c2666f8f17
Dustin Davis <1dustindavis@gmail.com>
2026-09-13 11:39:02 -0700

    Use TextBox abstraction for catalog technical text

--- gorilla-ui/tests/Gorilla.UI.App.WindowsUiTests/CatalogShellDriver.cs
+++ gorilla-ui/tests/Gorilla.UI.App.WindowsUiTests/CatalogShellDriver.cs
@@ -36,8 +36,7 @@ internal sealed class CatalogShellDriver
     public string OpenAndReadTechnicalDetails()
     {
         _session.WaitFor(() => ById("CatalogTechnicalDetails")).Click();
-        var content = _session.WaitFor(() => ById("CatalogTechnicalDetailsContent"));
-        return SafeValueOrName(content);
+        return _session.WaitFor(() => ById("CatalogTechnicalDetailsContent")).AsTextBox().Text;
     }
 
     public void WaitForFreshnessContaining(string expected, TimeSpan? timeout = null)
@@ -80,21 +79,6 @@ internal sealed class CatalogShellDriver
 
     private AutomationElement? ById(string automationId)
         => _session.MainWindow.FindFirstDescendant(cf => cf.ByAutomationId(automationId));
-
-    private static string SafeValueOrName(AutomationElement element)
-    {
-        try
-        {
-            if (element.Patterns.Value.IsSupported)
-            {
-                return element.Patterns.Value.Pattern.Value.Value;
-            }
-        }
-        catch (PropertyNotSupportedException)
-        {
-        }
-        return SafeName(element);
-    }
 
     private static string SafeName(AutomationElement element)
     {
